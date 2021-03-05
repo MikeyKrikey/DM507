ecae8467b1d2cc9abcb1c440b160c4012d4a6a80
habru19 <76167824+habru19@users.noreply.github.com>
2021-03-05 11:18:39 +0100

    Update PQHeap.py

--- PQHeap.py
+++ PQHeap.py
@@ -1,3 +1,18 @@
 Test
 
 hej
+
+def HeapMaximum(A):
+    return A[1]
+  
+def HeapExtractMax(A):
+    if A.heapsize < 1:
+        ValueError('Heap underflow')
+    
+    max = A[1]
+    A[1] = A[A.heapsize]
+    A.heapsize = A.heapsize - 1
+    MaxHeapify(A,1)
+    return max
+  
+
