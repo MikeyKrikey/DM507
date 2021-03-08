09d9039edff22e88b0950ee1fe99ea06bcf04ed8
MikeyKrikey <76166868+MikeyKrikey@users.noreply.github.com>
2021-03-08 12:31:00 +0100

    Update PQHeap.py

--- PQHeap.py
+++ PQHeap.py
@@ -1,70 +1,47 @@
-#asodifaosdjfo
+import math
 
-# Side 152
+# Side 152 (skiftede til definerede)
 def Parent(i):
-    return i // 2
+    return math.floor((i-1) / 2)
 
 def Left(i):
-    return 2*i
-
-def Right(i):
     return 2*i + 1
 
-# Side 154
-def maxHeapify(A,i):
+def Right(i):
+    return 2*i + 2
+
+# min-heapify
+def minHeapify(A,i):
     l = Left(i)
     r = Right(i)
 
-    if (l <= A.heapsize and A[l] > A[i]):
-        largest = l
-    else largest = i
+    if (l <= len(A)-1 and A[l] < A[i]):
+        smallest = l
+    else:
+        smallest = i
 
-    if (r <= A.heapsize and A[r] > A[largest]):
-        largest = r
+    if (r <= len(A)-1 and A[r] < A[smallest]):
+        smallest = r
     
-    if largest != i:
-        A[i], A[largest] = A[largest], A[i]
-        maxHeapify(A,largest)
-
-# Side 163
-def heapMaximum(A):
-    return A[1]
-
-def heapExtractMax(A):
-    if A.heapsize < 1:
-        ValueError('Heap underflow')
-    
-    maximum = A[1]
-    A[1] = A[A.heapsize]
-    A.heapsize = A.heapsize - 1
-    maxHeapify(A,1)
-    return maximum
-
-# Side 164
-def heapIncreaseKey(A,i,key):
-    if key < A[i]:
-        ValueError('new key is smaller than current key')
-    
-    A[i] = key
-
-    while i > 1 and A[Parent[i]] < A[i]:
-        A[i], A[Parent(i)] = A[Parent(i)], A[i]
-        i = Parent(i)
-
-def maxHeapInsert(A,key):
-    A.heapsize = A.heapsize + 1
-    A[A.heapsize] = -infty
-    heapIncreaseKey(A,A.heapsize,key)
+    if smallest != i:
+        A[i], A[smallest] = A[smallest], A[i]
+        minHeapify(A,smallest)
 
 # Vores implementering
 def extractMin(A):
-    # Fjerner det element med mindst prioritet i prioritetskøen A og returnerer det
-
+    minimum = A[0]
+    A[0] = A[len(A)-1]
+    A.pop()
+    minHeapify(A,0)
+    return minimum
 
 def insert(A,e):
-    # Indsætter elementet e i prioritetskøen A
+    A.append(e)
+    i = len(A) - 1
+    while i > 0 and A[Parent(i)] > A[i]:
+        A[i], A[Parent(i)] = A[Parent(i)], A[i]
+        i = Parent(i)
 
 def createEmptyPQ():
-    # Returnerer en ny, tom prioritetskø (en tom liste)
+    return []
   
-
