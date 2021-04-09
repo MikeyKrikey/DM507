c8c56c65767e058e3341bdbbc65d007553dd62d6
MikeyKrikey <76166868+MikeyKrikey@users.noreply.github.com>
2021-04-09 13:16:27 +0200

    Update DictBinTree.py

--- DictBinTree.py
+++ DictBinTree.py
@@ -9,102 +9,56 @@ Hanna Bruun-Schmidt, habru19
 
 # Program skal implementere datastrukturen binært søgetræ med tal som nøgler
 
-"""
-p. 290 bogens pseudo-kode for tree-search.
 
-Tree-Search(x,k)
-    if x == NIL or k == x.key:
-        return x
-    if k < x.key:
-        return Tree-Search(x.left, k)
-    else:
-        return Tree-Search(x.right,k)
-
-eller p.291 iterative-tree-search
-
-Iterative-Tree-Search(x,k)
-    while x != NIL and k != x.key:
-        if k < x.key:
-            x = x.left
-        else:
-            x = x.right
-    return x
-"""
 # Vores implementering af search
+# Skal returnere en boolean, der angiver om nøglen k er i træet T.
 def search(T,k):
     if T[0] == None:
         return False
     if k == T[0][0]:
         return True
     if k < T[0][0]:
-        return search([T[0][1]], k)
+        return search([T[0][1]], k) #rekursivt, kigger på venstre undertræ
     else:
-        return search([T[0][2]], k)
-    # Skal returnerer en boolean, der angiver om nøglen k er i træet T.
+        return search([T[0][2]], k) #rekursivt kigger på højre undertræ
+    
 
-"""
-p. 294 bogens pseudo-kode for tree-insert.
 
-Tree-Insert(T,z)
-    y = NIL 
-    x = T.root
-
-    while x != NIL:
-        y = x
-        if z.key < x.key:
-            x = x.left
-        else: 
-            x = x.right
-    z.p = y
-
-    if y == NIL:
-        T.root = z // tree T was empty
-    elseif z.key < y.key:
-        y.left = z
-    else:
-        y.right = z
-"""
 # vores implementering af insert
+# indsætter nøglen k i træet T.
 def insert(T,k):
     y = None 
-    k = [k, None, None]
+    k = [k, None, None] #lad nøglen k være en knude med to tomme undertræer
     x = T[0]
     while x != None:
         y = x
         if k[0] < x[0]:
-            x = x[1]
+            x = x[1] #hvis nøglen er mindre end x, så kig på venstre undertræ
         else: 
-            x = x[2]
+            x = x[2] #hvis nøglen er større end x, så kig på højre undertræ
     if y == None:
-        T[0] = k
+        T[0] = k #indsæt k som rod, hvis træet er tomt
     elif k[0] < y[0]:
-        y[1] = k
+        y[1] = k #indsæt k til venstre for bladet y, hvis mindre
     else:
-        y[2] = k
+        y[2] = k #indsæt k til højre for bladet y, hvis større
     
-    # indsætter nøglen k i træet T.
+    
 
-"""
-p. 288 bogens pseudo-kode af inorder-tree-walk
 
-Inorder-Tree-Walk(x)
-    if x != NIL:
-        Inorder-Tree-Walk(x.left)
-        print x.key
-        Inorder-Tree-Walk(x.right)
-"""
 
 # vores implementering af inorder gennemløb (Træet skal ikke holdes balanceret)
+# returnerer en liste med nøglerne i træet T i sorteret orden (fremfor at printe dem på skærmen som i bogens pseudo-kode)
 def orderedTraversal(T, liste = []):
     if T[0] != None:
-        orderedTraversal([T[0][1]], liste)
-        liste.append(T[0][0])
-        orderedTraversal([T[0][2]], liste)
+        orderedTraversal([T[0][1]], liste) #rekursivt, kig på venstre undertræ efter mindste element
+        liste.append(T[0][0]) #indsæt element i liste
+        orderedTraversal([T[0][2]], liste) #rekursivt kig på højre undertræ efter mindste element
     return liste
-    # returnerer en liste med nøglerne i træet T i sorteret orden (fremfor at printe dem på skærmen som i bogens pseudo-kode)
+    
 
+# returnerer et nyt tomt træ
 def createEmptyDict():
     return [None]
-    # returnerer et nyt tomt træ
 
 
