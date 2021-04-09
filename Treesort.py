976a73ad63c7466cd099bdc7cd5baf228e359ccf
MikeyKrikey <76166868+MikeyKrikey@users.noreply.github.com>
2021-04-09 12:52:07 +0200

    Update Treesort.py

--- Treesort.py
+++ Treesort.py
@@ -7,4 +7,17 @@ Hanna Bruun-Schmidt, habru19
 
 '''
 
-# Skal bruge funktionerne fra DictBinTree
+import sys
+import DictBinTree
+
+dictionary = DictBinTree.createEmptyDict()
+
+n = 0
+for line in sys.stdin:
+    DictBinTree.insert(dictionary,int(float(line)))
+    n = n+1
+
+print()
+while n > 0:
+    print(DictBinTree.orderedTraversal(dictionary)[n-1])
+    n = n-1
